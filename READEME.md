40aa3fa8741772cfe175b49dfce9a7ac793153ac
VeryverySoHappy <frontEnd0815@gmail.com>
2022-03-13 23:42:43 +0900

    footer

--- READEME.md
+++ READEME.md
@@ -7,3 +7,7 @@
 - 3번째 폰트어썸 말고 다른거 사용할지
 
 - 돌 사진 넣을지
+
+- <a href="http://www.nia.or.kr/files/ko/e-catalogue/kor/index.html" target="_blank" title="새창" class="bottom-banner" tabindex="-1"><img src="/files/link/4cabeaa8-6dc8-4828-842c-ac689c4cc31e.jpg" alt="그린 IT 생활실천 수칙"></a>
+
+a태그에 target, title 꼭 넣어야되는지
---
+++ js/footer.js
@@ -0,0 +1,38 @@
+var selectNum=0;
+function changeItem(num){
+  if($('.slide').is(':animated')==false){
+
+    $('.page_navi li.on').removeClass('on')
+    $('.page_navi li').eq(num).addClass('on')
+
+    $('.slide').animate({
+      top:-(num * 196) + 'px'
+    },3000,'easeOutBack');
+  }
+}
+$(document).ready(function(){
+  $('.page_navi li').on('click', function(e){
+    e.preventDefault()        
+    selectNum=$(this).index()
+    changeItem(selectNum)
+  })
+
+  $('.next').on('click', function(e){
+    e.preventDefault()
+    selectNum++
+    if(selectNum>$('.page_navi li').length-1){
+      selectNum=0;
+    }
+    changeItem(selectNum)
+  })
+  
+
+  $('.prev').on('click', function(e){
+    e.preventDefault()
+    selectNum--
+    if(selectNum<0) {
+      selectNum=$('.page_navi li').length-1
+    }
+    changeItem(selectNum)
+  })
+})
